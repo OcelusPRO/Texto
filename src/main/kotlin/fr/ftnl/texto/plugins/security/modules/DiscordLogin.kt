61cb98ec2b183434b41a66c0c9d6c40927be6bfd
ocelusPRO <32218885+OcelusPRO@users.noreply.github.com>
2024-03-15 15:14:05 +0100

    add discord oAuth login config

---
+++ src/main/kotlin/fr/ftnl/texto/plugins/security/modules/DiscordLogin.kt
@@ -0,0 +1,31 @@
+package fr.ftnl.texto.plugins.security.modules
+
+import fr.ftnl.texto.HTTP_CLIENT
+import fr.ftnl.texto.plugins.UserSession
+import io.ktor.http.*
+import io.ktor.server.auth.*
+import io.ktor.server.config.*
+import io.ktor.server.sessions.*
+
+fun AuthenticationConfig.discordLogin(cfg: ApplicationConfig) {
+    oauth("discord_oauth") {
+        skipWhen { call ->
+            val session = call.sessions.get<UserSession>()
+            session?.discordUser != null
+        }
+
+        urlProvider = { cfg.property("texto.baseUrl").getString() + "/clb" }
+        providerLookup = {
+            OAuthServerSettings.OAuth2ServerSettings(
+                name = "discord",
+                authorizeUrl = "https://discord.com/oauth2/authorize",
+                accessTokenUrl = "https://discord.com/api/oauth2/token",
+                requestMethod = HttpMethod.Post,
+                clientId = cfg.property("security.discordOauth.clientId").getString(),
+                clientSecret = cfg.property("security.discordOauth.clientSecret").getString(),
+                defaultScopes = listOf("identify", "connections"),
+            )
+        }
+        client = HTTP_CLIENT
+    }
+}
