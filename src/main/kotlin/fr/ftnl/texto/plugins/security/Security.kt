cd63c0db436d414620a8a41a615097d2c6d97c83
ocelusPRO <32218885+OcelusPRO@users.noreply.github.com>
2024-03-15 15:17:15 +0100

    add discord login

--- src/main/kotlin/fr/ftnl/texto/plugins/security/Security.kt
+++ src/main/kotlin/fr/ftnl/texto/plugins/security/Security.kt
@@ -1,12 +1,14 @@
 package fr.ftnl.texto.plugins.security
 
 import fr.ftnl.texto.plugins.security.modules.apiSecurityConfig
+import fr.ftnl.texto.plugins.security.modules.discordLogin
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 
 fun Application.configureSecurity() {
     install(Authentication) {
         apiSecurityConfig()
+        discordLogin(this@configureSecurity.environment.config)
     }
 }
 
