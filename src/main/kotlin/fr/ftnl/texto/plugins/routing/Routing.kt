5879927860d6ce42be47112c6273b6f9e88430e8
ocelusPRO <32218885+OcelusPRO@users.noreply.github.com>
2024-03-15 15:16:47 +0100

    add rateslimites

--- src/main/kotlin/fr/ftnl/texto/plugins/routing/Routing.kt
+++ src/main/kotlin/fr/ftnl/texto/plugins/routing/Routing.kt
@@ -1,25 +1,23 @@
 package fr.ftnl.texto.plugins.routing
 
-import com.github.benmanes.caffeine.cache.Cache
-import com.github.benmanes.caffeine.cache.Caffeine
-import fr.ftnl.texto.database.models.Texto
-import fr.ftnl.texto.ext.md5
 import fr.ftnl.texto.plugins.UserSession
+import fr.ftnl.texto.plugins.routing.authRoutes.discordLoginRoute
+import fr.ftnl.texto.plugins.routing.pages.getPage
+import fr.ftnl.texto.plugins.routing.pages.postPage
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.config.*
 import io.ktor.server.http.content.*
-import io.ktor.server.mustache.*
 import io.ktor.server.plugins.autohead.*
 import io.ktor.server.plugins.doublereceive.*
+import io.ktor.server.plugins.ratelimit.*
 import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.request.*
 import io.ktor.server.resources.Resources
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.sessions.*
-import java.io.File
-import kotlin.time.Duration.Companion.minutes
-import kotlin.time.toJavaDuration
+import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureRouting(config: ApplicationConfig) {
     install(AutoHeadResponse)
@@ -30,68 +28,31 @@ fun Application.configureRouting(config: ApplicationConfig) {
         status(HttpStatusCode.NotFound) { call, status -> call.respondText(text = "404: Page Not Found", status = status) }
 
         exception<Throwable> { call, cause ->
+            cause.printStackTrace()
             call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
         }
     }
+    install(RateLimit) {
+        register(RateLimitName("post_texto_user")) {
+            rateLimiter(limit = 2, refillPeriod = 60.seconds)
+            requestKey { applicationCall ->
+                applicationCall.sessions.get<UserSession>()!!
+            }
+        }
+        register(RateLimitName("post_texto_api")) {
+            rateLimiter(limit = 30, refillPeriod = 60.seconds)
+            requestKey { applicationCall ->
+                val header = applicationCall.application.environment.config.property("security.api.header").getString()
+                applicationCall.request.header(header)!!
+            }
+        }
+    }
+
     routing {
-        fun getPageInfo(pageId: String): PageInfo? {
-            val file = File("./pages/$pageId")
-            if (file.exists().not()) return null
+        discordLoginRoute()
+        getPage()
+        postPage()
 
-            val texto = Texto.get(pageId)
-
-            var badPage = false
-            when {
-                texto == null -> { badPage = true }
-                texto.expireAt?.isBeforeNow == true -> { badPage = true }
-            }
-            if (badPage) {
-                file.delete()
-                texto?.deleteOnTransaction()
-                return null
-            }
-
-            val info = PageInfo(
-                TextoInfo(
-                    content = file.readText(),
-                    title = texto!!.name,
-                    description = texto.description,
-                    vues = texto.vues +1
-                ),
-                AuthorInfo(
-                    avatar = texto.author.avatarUrl,
-                    name = texto.author.name,
-                    social = texto.author.social.map { SocialMedia(it.url, it.iconName) },
-                    vues = texto.author.textos.sumOf { it.vues }+1,
-                    texto = texto.author.textos.size
-                )
-            )
-            return info
-        }
-        val pageCache: Cache<String, PageInfo?> = Caffeine.newBuilder()
-            .expireAfterWrite(5.minutes.toJavaDuration())
-            .removalListener { key: String?, value: PageInfo?, cause ->
-                if (key == null) return@removalListener
-                if (value == null) return@removalListener
-                val texto = Texto.get(key) ?: return@removalListener
-                texto.vues = value.texto.vues
-            }
-            .build()
-
-
-        get("/{texto_id}"){
-            val textoId = call.parameters["texto_id"]?.md5() ?: return@get call.respond(HttpStatusCode.NotFound)
-            val info = pageCache.get(textoId, ::getPageInfo) ?: return@get call.respond(HttpStatusCode.NotFound)
-
-            val session = call.sessions.get<UserSession>() ?: UserSession()
-            if(session.pageViews.contains(textoId).not()){
-                info.texto.vues++
-                info.author.vues++
-                pageCache.put(textoId, info)
-            }
-            session.pageViews.add(textoId)
-            call.respondTemplate("code.hbs", info)
-        }
 
         staticResources("/static", "static")
         staticResources("/", "static/pages")
