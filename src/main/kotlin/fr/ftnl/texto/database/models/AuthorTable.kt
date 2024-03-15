f2bab4a3d9b6bab1efbe0ee696e6346c6e168767
ocelusPRO <32218885+OcelusPRO@users.noreply.github.com>
2024-03-15 15:13:10 +0100

    add get and create function for author + generate api key

--- src/main/kotlin/fr/ftnl/texto/database/models/AuthorTable.kt
+++ src/main/kotlin/fr/ftnl/texto/database/models/AuthorTable.kt
@@ -1,8 +1,10 @@
 package fr.ftnl.texto.database.models
 
+import com.sun.jna.platform.unix.solaris.LibKstat.KstatNamed.UNION.STR
 import fr.ftnl.texto.database.abstract.BaseIntEntity
 import fr.ftnl.texto.database.abstract.BaseIntEntityClass
 import fr.ftnl.texto.database.abstract.BaseIntIdTable
+import fr.ftnl.texto.ext.keyGen
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -10,11 +12,26 @@ object AuthorTable : BaseIntIdTable("TBL_AUTHOR_AUT") {
     val discordId = long("discord_user_id").uniqueIndex()
     val name = varchar("name", 25)
     val avatarUrl = varchar("avatar", 255)
+    val apiKey = varchar("api_key", 255)
 }
 
 class Author(id: EntityID<Int>): BaseIntEntity(id, AuthorTable) {
     companion object : BaseIntEntityClass<Author>(AuthorTable) {
+        fun get(id: Long) = transaction {
+            find { AuthorTable.discordId eq id }.firstOrNull()
+        }
+        fun get(key: String) = transaction {
+            find { AuthorTable.apiKey eq key }.firstOrNull()
+        }
 
+        fun create(did: Long, name: String, avatar: String?) = transaction {
+            new {
+                _discordId = did
+                _name = name
+                _avatarUrl = avatar ?: "/static/images/Unknown_person.jpg"
+                _apiKey = String.keyGen(255)
+            }
+        }
     }
 
 
@@ -23,12 +40,19 @@ class Author(id: EntityID<Int>): BaseIntEntity(id, AuthorTable) {
         get() = transaction { _discordId }
 
     private var _name by AuthorTable.name
-    val name: String
+    var name: String
         get() = transaction { _name }
+        set(value) = transaction { _name = value }
 
     private var _avatarUrl by AuthorTable.avatarUrl
-    val avatarUrl: String
+    var avatarUrl: String
         get() = transaction { _avatarUrl }
+        set(value) = transaction { _avatarUrl = value }
+
+    private var _apiKey by AuthorTable.apiKey
+    var apiKey: String
+        get() = transaction { _apiKey }
+        set(value) = transaction { _apiKey = value }
 
     private val _social by SocialMedia referrersOn SocialMediaTable.user
     val social
