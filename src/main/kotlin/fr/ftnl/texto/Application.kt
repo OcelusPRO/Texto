363b909e544fc01a67dd42e71a3d63017a637c31
ocelusPRO <32218885+OcelusPRO@users.noreply.github.com>
2024-03-15 15:12:43 +0100

    add http client

--- src/main/kotlin/fr/ftnl/texto/Application.kt
+++ src/main/kotlin/fr/ftnl/texto/Application.kt
@@ -6,11 +6,18 @@ import fr.ftnl.texto.plugins.routing.configureHTTP
 import fr.ftnl.texto.plugins.routing.configureRouting
 import fr.ftnl.texto.plugins.routing.configureTemplating
 import fr.ftnl.texto.plugins.security.configureSecurity
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.http.*
 import io.ktor.server.application.*
+
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
+
+val HTTP_CLIENT = HttpClient(CIO){}
 
 fun Application.module() {
     startup()
