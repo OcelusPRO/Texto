c53dddbfacd9e376f3011ad4a1ea11a10d166f1f
ocelusPRO <32218885+OcelusPRO@users.noreply.github.com>
2024-03-17 10:59:37 +0100

    implement ratelimites

--- src/main/kotlin/fr/ftnl/texto/plugins/routing/Routing.kt
+++ src/main/kotlin/fr/ftnl/texto/plugins/routing/Routing.kt
@@ -32,17 +32,24 @@ fun Application.configureRouting(config: ApplicationConfig) {
             call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
         }
     }
+    val cfg = environment.config
     install(RateLimit) {
         register(RateLimitName("post_texto_user")) {
-            rateLimiter(limit = 2, refillPeriod = 60.seconds)
+            rateLimiter(
+                limit = cfg.property("texto.ratesLimites.userLimite").getString().toInt(),
+                refillPeriod = cfg.property("texto.ratesLimites.userRefillSeconds").getString().toInt().seconds
+            )
             requestKey { applicationCall ->
                 applicationCall.sessions.get<UserSession>()!!
             }
         }
         register(RateLimitName("post_texto_api")) {
-            rateLimiter(limit = 30, refillPeriod = 60.seconds)
+            rateLimiter(
+                limit = cfg.property("texto.ratesLimites.apiLimite").getString().toInt(),
+                refillPeriod = cfg.property("texto.ratesLimites.apiRefillSeconds").getString().toInt().seconds
+            )
             requestKey { applicationCall ->
-                val header = applicationCall.application.environment.config.property("security.api.header").getString()
+                val header = cfg.property("security.api.header").getString()
                 applicationCall.request.header(header)!!
             }
         }
@@ -57,45 +64,4 @@ fun Application.configureRouting(config: ApplicationConfig) {
         staticResources("/static", "static")
         staticResources("/", "static/pages")
     }
-}
-
-data class PageInfo(
-    val texto: TextoInfo = TextoInfo(),
-    val author: AuthorInfo = AuthorInfo()
-)
-data class TextoInfo(
-    val content: String = "",
-    val title: String = "",
-    val description: String = "",
-    var vues: Int = 0
-)
-private fun String.toClean() = this.lowercase().replace(" ", "_")
-data class AuthorInfo(
-    val avatar: String = "",
-    val name: String = "",
-    val cleanName: String = "",
-    val social: String = "",
-    var vues: Int = 0,
-    val textos: Int = 0
-) {
-    constructor(
-        avatar: String,
-        name: String,
-        social: List<SocialMedia>,
-        vues: Int,
-        texto: Int
-    ) : this(
-        avatar,
-        name,
-        name.toClean(),
-        social.map { it.toHtmlMedia() }.joinToString { it },
-        vues,
-        texto
-    )
-}
-data class SocialMedia(
-    val url: String = "",
-    val icon: String = ""
-){
-    fun toHtmlMedia() = "<a href=\"$url\"><i class=\"fa fa-$icon\"></i></a>"
 }
