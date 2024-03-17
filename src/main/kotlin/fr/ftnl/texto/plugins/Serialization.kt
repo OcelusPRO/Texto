647b9890e51bc2b0a13ec3f636dd3cab6df7ce83
ocelusPRO <32218885+OcelusPRO@users.noreply.github.com>
2024-03-17 10:59:52 +0100

    add gson serializer

--- src/main/kotlin/fr/ftnl/texto/plugins/Serialization.kt
+++ src/main/kotlin/fr/ftnl/texto/plugins/Serialization.kt
@@ -1,11 +1,17 @@
 package fr.ftnl.texto.plugins
 
+import io.ktor.serialization.gson.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import java.text.DateFormat
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         json()
+        gson {
+            setDateFormat(DateFormat.LONG)
+            setPrettyPrinting()
+        }
     }
 }
