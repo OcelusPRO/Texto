1f83184f2fa483f29f39df89c007d8b5d7bcabe2
ocelusPRO <32218885+OcelusPRO@users.noreply.github.com>
2024-03-19 20:57:04 +0100

    add login method

--- src/main/kotlin/fr/ftnl/texto/plugins/security/Security.kt
+++ src/main/kotlin/fr/ftnl/texto/plugins/security/Security.kt
@@ -1,7 +1,6 @@
 package fr.ftnl.texto.plugins.security
 
-import fr.ftnl.texto.plugins.security.modules.apiSecurityConfig
-import fr.ftnl.texto.plugins.security.modules.discordLogin
+import fr.ftnl.texto.plugins.security.modules.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 
@@ -9,6 +8,9 @@ fun Application.configureSecurity() {
     install(Authentication) {
         apiSecurityConfig()
         discordLogin(this@configureSecurity.environment.config)
+        twitchLogin(this@configureSecurity.environment.config)
+        formAuth(this@configureSecurity.environment.config)
+        sessionAuth()
     }
 }
 
