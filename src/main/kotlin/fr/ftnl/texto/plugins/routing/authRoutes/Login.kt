2fd52eba7ff75496ce04c023846a1aef008c7123
ocelusPRO <32218885+OcelusPRO@users.noreply.github.com>
2024-03-20 20:13:43 +0100

    add register route

--- src/main/kotlin/fr/ftnl/texto/plugins/routing/authRoutes/Login.kt
+++ src/main/kotlin/fr/ftnl/texto/plugins/routing/authRoutes/Login.kt
@@ -8,8 +8,10 @@ import fr.ftnl.texto.plugins.routing.pages.UserInfo
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.mustache.*
+import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.routing.get
 import io.ktor.server.sessions.*
 import kotlinx.serialization.Serializable
 
@@ -36,6 +38,26 @@ fun  Route.login() {
 
 }
 
+fun Route.register(){
+    route("/form") { formRegisterRoute() }
+    get {
+        val info = LoginData()
+        val userSession = call.sessions.get<UserSession>()
+        if (userSession?.connectedEmailHash != null) {
+            val author = userSession.connectedEmailHash.let { Author.getByEmailHash(it) }
+            info.connected = UserInfo(
+                author?.name ?: "",
+                author?.avatarUrl ?: "",
+                true,
+                false
+            )
+        } else info.connected = UserInfo()
+        val baseUrl = call.application.environment.config.property("texto.baseUrl").getString()
+        info.website.baseUrl = baseUrl
+        call.respondTemplate("register.hbs", info)
+    }
+}
+
 @Serializable
 data class LoginData(
     var connected: UserInfo = UserInfo(),
@@ -43,5 +65,6 @@ data class LoginData(
 )
 @Serializable
 data class WebsiteInfo(
-    var baseUrl: String = ""
+    var baseUrl: String = "",
+    var error: String = ""
 )
